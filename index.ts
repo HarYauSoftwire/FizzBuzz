aa7d1b800ac1acdb30ed41d98163dd62029719c9
Harvey Yau <Harvey.Yau@softwire.com>
2021-07-06 14:06:31 +0100

    Add features 7 to 17, add user input

--- index.ts
+++ index.ts
@@ -1,14 +1,42 @@
-for (let index: number = 1; index <= 100; index++) {
-    let word: string = '';
+// for (let index: number = 1; index <= 105; index++) {
+//     console.log(getFizzBuzzWord(index));
+// }
+
+function getFizzBuzzWord(index: number) : string {
+    let answerList: string[] = [];
     if (index % 3 == 0) {
-        word += 'Fizz';
+        answerList.push('Fizz');
     }
     if (index % 5 == 0) {
-        word += 'Buzz';
+        answerList.push('Buzz');
     }
-    
-    if (word == '') {
-        word = index.toString();
+    if (index % 7 == 0) {
+        answerList.push('Bang');
     }
-    console.log(word);
+    if (index % 11 == 0) {
+        answerList = ['Bong'];
+    }
+    if (index % 13 == 0) {
+        let bIndex = answerList.findIndex((element) => element[0] == 'B');
+        if (bIndex == -1) {
+            answerList.push('Fezz');
+        }
+        else {
+            answerList.splice(bIndex, 0, 'Fezz');
+        }
+    }
+    if (index % 17 == 0) {
+        answerList.reverse();
+    }
+
+    if (answerList.length == 0) {
+        return index.toString();
+    }
+    else {
+        return answerList.join('');
+    }
 }
+
+const promptFunc = require('prompt-sync')();
+const inNumString = promptFunc('Please enter a number...');
+console.log(getFizzBuzzWord(Number(inNumString)));
