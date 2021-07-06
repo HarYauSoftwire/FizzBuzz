aec704442cd86a0c170dfdef703e4e6ae2de8fde
Harvey Yau <Harvey.Yau@softwire.com>
2021-07-06 12:19:56 +0100

    basic FizzBuzz program

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log('Hello, world!');
+for (let index: number = 1; index <= 100; index++) {
+    let word: string = '';
+    if (index % 3 == 0) {
+        word += 'Fizz';
+    }
+    if (index % 5 == 0) {
+        word += 'Buzz';
+    }
+    
+    if (word == '') {
+        word = index.toString();
+    }
+    console.log(word);
+}
